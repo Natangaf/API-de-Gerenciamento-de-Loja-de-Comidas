a70f28b840550831e0b01635fa3f81c5193a33ed
natabgaf <natanfernandes52@gmail.com>
2023-05-30 12:28:15 -0300

    feat: tarefa Refatorando finalizada

--- management/__init__.py
+++ management/__init__.py
@@ -1,2 +1,2 @@
-from management.product_handler import get_product_by_id, get_products_by_type, add_product
+from management.product_handler import get_product_by_id, get_products_by_type, add_product ,menu_report
 from management.tab_handler import calculate_tab
--- management/product_handler.py
+++ management/product_handler.py
@@ -2,20 +2,25 @@ from menu import products
 
 
 def get_product_by_id(id):
-    for dicionario in products:
-        if id in dicionario.values():
-            return dicionario
-    return {}
+    try:
+        for dicionario in products:
+            if id in dicionario.values():
+                return dicionario
+        return {}
+    except TypeError:
+        print("product id must be an int")
 
 
 def get_products_by_type(type):
+    try:
+        response = []
 
-    response = []
-
-    for dicionario in products:
-        if type in dicionario.values():
-            response.append(dicionario)
-    return response
+        for dicionario in products:
+            if type in dicionario.values():
+                response.append(dicionario)
+        return response
+    except TypeError:
+        print("product type must be a str")
 
 
 def add_product(products, **menu):
@@ -30,3 +35,26 @@ def add_product(products, **menu):
     products.append(menu)
 
     return menu
+
+
+def menu_report():
+    product_count = len(products)
+    average_price = 0
+    type_product = {}
+    for product in products:
+        average_price += product['price']
+        if product['type'] not in type_product:
+            type_product[product['type']] = 1
+        else:
+            type_product[product['type']] += 1
+
+    average_price =round(average_price / product_count,2)
+
+    most_common_type = ''
+    cont = 0
+    for key, value in type_product.items():
+        if value > cont:
+            most_common_type = key
+            cont = value
+
+    return f"Products Count: {product_count} - Average Price: ${average_price} - Most Common Type: {most_common_type}"
